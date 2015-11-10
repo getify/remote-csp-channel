dcf5f32d00049f87541b8716c600da9a6741174a
Kyle Simpson <getify@gmail.com>
2015-11-09 20:44:05 -0600

    rearranging so all the protocol logic is in the core bridge, and the web worker stuff alone is in the adapter

--- bridge_webworker/index.js
+++ bridge_webworker/index.js
@@ -5,13 +5,9 @@
 })("BridgeWebWorker",this,function DEF(name,context){
 	"use strict";
 
-	var remote_csp,
-		orig_csp,
-		bridge_context,
-		wrap_return,
+	var bridge_context,
 		publicAPI = {
-			setup: setup,
-			connect: connectBridge
+			connect: connect
 		};
 
 	return publicAPI;
@@ -19,22 +15,8 @@
 
 	// *******************************
 
-	function setup(remote,origCSP,wrapReturn) {
-		remote_csp = remote;
-		orig_csp = Object.assign({},origCSP);
-		remote_csp.hijackCSP(origCSP);
-		wrap_return = wrapReturn || function $$promisewrap(v) { return Promise.resolve(v); };
-	}
-
-	function connectBridge(bridgeName,workerObj) {
-		var worker, msg_target, send_queue = [],
-			channels = {}, message_ids = {};
-
-		remote_csp.defineBridge(bridgeName,{
-			open: openChannel,
-			close: closeChannel,
-			signal: signalChannel
-		});
+	function connect(workerObj) {
+		var worker, msgHandler, send_queue = [], msg_target;
 
 		if (typeof window == "undefined") {
 			worker = true;
@@ -42,206 +24,37 @@
 			// in shared worker?
 			if ("onconnect" in self) {
 				bridge_context = "shared-worker";
-				self.addEventListener("connect",WWconnect,false);
+				self.addEventListener("connect",onConnect,false);
 			}
 			// assume regular worker
 			else {
 				bridge_context = "regular-worker";
-				self.addEventListener("message",WWstart,false);
+				self.addEventListener("message",onStart,false);
 			}
 		}
 		// assume main window
 		else {
-			bridge_context = "browser";
+			bridge_context = "main";
 			worker = workerObj;
-			WWsetup(worker,worker);
-			WWmessageTo({ start: true, "msg-source": bridge_context });	// TODO: remove `msg-source`?
+			setup(worker,worker);
+			sendMessage({ start: true, "msg-source": bridge_context });	// TODO: remove `msg-source`?
 		}
+
+		return {
+			onMessage: defineMessageHandler,
+			sendMessage: sendMessage
+		};
 
 
 		// *******************************
 
-		function openChannel(ch) {
-			var channel_id = ch.remote[1];
-
-			// first time this channel-ID is trying to be opened?
-			if (!channels[channel_id] || channels[channel_id].resolve) {
-				// no local channel entry yet?
-				if (!channels[channel_id]) {
-					channels[channel_id] = channelPendingEntry({ ch: ch });
-				}
-				// otherwise, remote channel connection already received
-				else {
-					channels[channel_id].ch = ch;
-					channels[channel_id].resolve(ch);
-					channels[channel_id].resolve = channels[channel_id].reject = null;
-				}
-
-				WWmessageTo({
-					"remote-connected": channel_id,
-					"msg-source": bridge_context		// TODO: remove `msg-source`?
-				});
-			}
-
-			if (!ch.pending_messages) {
-				ch.pending_messages = [];
-			}
-
-			return channels[channel_id].pr;
+		function defineMessageHandler(handler) {
+			msgHandler = handler;
 		}
 
-		function closeChannel(channelID) {
-			WWmessageTo({
-				"remote-closed": channelID
-			});
-		}
+		function sendMessage(msg) {
+			msg["msg-source"] = bridge_context;
 
-		function signalChannel(channelID,cspMethod) {
-			var inOut = /^put/.test(cspMethod) ? "out" : "in",
-				ch = channels[channelID].ch,
-				args = [].slice.call(arguments,2), args2,
-				entry, id, pr, pr2, ack, msg;
-
-			// found a matching (aka, opposite) entry?
-			entry = findOppositePendingMessageEntry(ch,inOut);
-			if (entry) {
-				id = entry.in || entry.out;
-
-				// local entry?
-				if (message_ids[id]) {
-					// cancel the previous remote pending message
-					WWmessageTo({
-						"remote-action-cancel": channelID,
-						"id": id
-					});
-
-					// find and remove this entry from the queue
-					findPendingMessageEntry(ch,id,/*remove=*/true);
-
-					// fake entry state as if remote signaling had completed
-					entry[inOut] = getNewMessageID(inOut + ":" + bridge_context);
-
-					args2 = [ch].concat(entry.waiting_for_ack.args);
-
-					// redo previously remoted action as local instead
-					pr = wrap_return(orig_csp[entry.waiting_for_ack.method].apply(null,args2));
-
-					// perform current CSP action as local
-					pr2 = wrap_return(orig_csp[cspMethod].apply(null,args));
-
-					// compose both local csp actions together
-					pr2
-						.then(
-							function onFulfilled(){ return pr; },
-							function onRejected(){ return pr; }
-						)
-						.then(entry.resolve,entry.reject)
-						.then(cleanupEntry,cleanupEntry);
-
-					return pr2;
-				}
-				// otherwise, prepare ACK to send back to remote
-				else {
-					ack = id;
-				}
-			}
-
-			if (!entry) {
-				// make a new (pending) local entry
-				entry = makePendingMessageEntry(ch,inOut);
-				id = entry.in || entry.out;
-			}
-			else {
-				// add local id to previous entry
-				id = getNewMessageID(inOut + ":" + bridge_context);
-				entry[inOut] = id;
-			}
-
-			// CSP method with channel as single first param
-			if (/^(?:put|take)/.test(cspMethod)) {
-				args.shift();
-			}
-			else {
-				// TODO: filter out channels from alts(..) / altsAsync(..) args
-			}
-
-			msg = {
-				"remote-action": channelID,
-				"id": id,
-				"method": cspMethod,
-				"args": args
-			};
-
-			// send ack along with message
-			if (ack) {
-				msg.ack = ack;
-			}
-
-			WWmessageTo(msg);
-
-			entry.waiting_for_ack = msg;
-
-			return entry.pr;
-
-
-			// *******************************
-
-			function cleanupEntry() {
-				ClearMessageEntry(entry);
-			}
-		}
-
-		function ClearMessageEntry(entry) {
-			delete message_ids[entry.in];
-			delete message_ids[entry.out];
-			entry.resolve = entry.reject = entry.pr = entry.waiting_for_ack =
-				entry.ack = entry.in = entry.out = null;
-		}
-
-		function findPendingMessageEntry(ch,msgID,remove) {
-			var inOut = msgID.match(/^(in|out):/)[1], entry;
-
-			for (var i=0; i<ch.pending_messages.length; i++) {
-				if (ch.pending_messages[i][inOut] == msgID) {
-					entry = ch.pending_messages[i];
-					if (remove) {
-						ch.pending_messages.splice(i,1);
-					}
-					return entry;
-				}
-			}
-		}
-
-		function findOppositePendingMessageEntry(ch,inOut) {
-			for (var i=0; i<ch.pending_messages.length; i++) {
-				if (!ch.pending_messages[i][inOut]) {
-					return ch.pending_messages[i];
-				}
-			}
-		}
-
-		function getNewMessageID(prefix) {
-			do { var id = prefix + ":" + Math.random(); }
-			while (id in message_ids);
-			message_ids[id] = true;
-			return id;
-		}
-
-		function makePendingMessageEntry(ch,inOut) {
-			var id = getNewMessageID(inOut + ":" + bridge_context);
-
-			var entry = { in: null, out: null };
-			entry[inOut] = id;
-			entry.pr = new Promise(function executor(resolve,reject){
-				entry.resolve = resolve;
-				entry.reject = reject;
-			});
-			ch.pending_messages.push(entry);
-
-			return entry;
-		}
-
-		function WWmessageTo(msg) {
 			if (!msg_target) {
 				send_queue.push(msg);
 			}
@@ -254,162 +67,30 @@
 			}
 		}
 
-		function WWmessageFrom(evt) {
-			var action, channel_id, msg, args, entry, inOut;
+		function onMessage(evt) {
+			var msg;
 
 			try {
 				msg = JSON.parse(evt.data);
 			}
 			catch (err) { return; }
 
-			if (("remote-connected" in msg) || ("remote-closed" in msg)) {
-				action = ("remote-connected" in msg) ?
-					"remote-connected" :
-					"remote-closed";
-				channel_id = msg[action];
-
-				// remote channel connection signal?
-				if (action == "remote-connected") {
-					// local channel entry not yet defined?
-					if (!channels[channel_id]) {
-						channels[channel_id] = channelPendingEntry({});
-					}
-					// local channel still pending on remote open?
-					else if (channels[channel_id].resolve) {
-						channels[channel_id].resolve(channels[channel_id].ch);
-						channels[channel_id].resolve = channels[channel_id].reject = null;
-					}
-					// otherwise, ignore connection message on already
-					// opened channel
-				}
-				// remote channel close signal received?
-				else if (action == "remote-closed" &&
-					channels[channel_id]
-				) {
-					// local channel to close?
-					if (channels[channel_id].ch) {
-						channels[channel_id].ch.origClose();
-					}
-					// local channel still pending?
-					else if (channels[channel_id].reject) {
-						channels[channel_id].reject();
-					}
-
-					// cleanup
-					channels[channel_id].resolve = channels[channel_id].reject = null;
-				}
-			}
-			else if (msg["remote-action"]) {
-				channel_id = msg["remote-action"];
-
-				// recognized local CSP method?
-				if (orig_csp[msg.method]) {
-					// check for opposite matching entry to attach to
-					inOut = msg.id.match(/^(in|out):/)[1];
-					entry = findOppositePendingMessageEntry(channels[channel_id].ch,inOut);
-					if (entry) {
-						// add remote msg id
-						entry[inOut] = msg.id;
-
-						// send ack
-						WWmessageTo({
-							"remote-action-ack": channel_id,
-							id: msg.id
-						});
-
-						// ack bundled in with message, and entry now complete?
-						if (msg.ack && entry.in && entry.out) {
-							// remove the entry
-							entry = findPendingMessageEntry(
-								channels[channel_id].ch,
-								msg.ack,
-								/*remove=*/true
-							);
-
-							// double check that we actually found/removed the entry?
-							if (entry) {
-								// add channel to list of args
-								args = [channels[channel_id].ch].concat(entry.waiting_for_ack.args);
-
-								// run CSP action for now-ack'd local msg
-								wrap_return(
-									orig_csp[entry.waiting_for_ack.method].apply(null,args)
-								)
-								.then(entry.resolve,entry.reject);
-
-								ClearMessageEntry(entry);
-							}
-						}
-					}
-					// no entry found, so make one to wait for local action
-					else {
-						entry = { in: null, out: null };
-						entry[inOut] = msg.id;
-						channels[channel_id].ch.pending_messages.push(entry);
-					}
-
-					// add channel to list of args
-					args = [channels[channel_id].ch].concat(msg.args);
-
-					// run CSP action from remote msg
-					orig_csp[msg.method].apply(null,args);
-				}
-			}
-			else if (msg["remote-action-ack"]) {
-				channel_id = msg["remote-action-ack"];
-
-				entry = findPendingMessageEntry(
-					channels[channel_id].ch,
-					msg.id
-				);
-
-				// entry complete now that is has ack and both in/out id's?
-				if (entry.in && entry.out) {
-					// remove the entry
-					entry = findPendingMessageEntry(
-						channels[channel_id].ch,
-						msg.id,
-						/*remove=*/true
-					);
-
-					// add channel to list of args
-					args = [channels[channel_id].ch].concat(entry.waiting_for_ack.args);
-
-					// run CSP action for now-ack'd local msg
-					wrap_return(
-						orig_csp[entry.waiting_for_ack.method].apply(null,args)
-					)
-					.then(entry.resolve,entry.reject);
-
-					ClearMessageEntry(entry);
-				}
-			}
-			else if (msg["remote-action-cancel"]) {
-				channel_id = msg["remote-action-cancel"];
-				if (channels[channel_id].ch) {
-					entry = findPendingMessageEntry(
-						channels[channel_id].ch,
-						msg["id"],
-						/*remove=*/true
-					);
-					ClearMessageEntry(entry);
-
-					// TODO: remove the local channel's queue action!
-				}
+			if (msgHandler) {
+				msgHandler(msg);
 			}
 		}
 
-		function WWsetup(source,target) {
+		function setup(source,target) {
 			if (target) {
 				msg_target = target;
 				if (send_queue.length > 0) {
-					send_queue.forEach(WWmessageTo);
+					send_queue.forEach(sendMessage);
 					send_queue.length = 0;
 				}
 			}
 
 			source.addEventListener("message",
-				msg_target ? WWmessageFrom : waitForMsgTarget,
+				msg_target ? onMessage : waitForMsgTarget,
 				false
 			);
 
@@ -417,43 +98,36 @@
 			// *******************************
 
 			function waitForMsgTarget(evt) {
+				source.removeEventListener("message",waitForMsgTarget,false);
+				source.addEventListener("message",onMessage,false);
+
 				msg_target = evt.source;
-				send_queue.forEach(WWmessageTo);
+				send_queue.forEach(sendMessage);
 				send_queue.length = 0;
 
-				source.removeEventListener("message",waitForMsgTarget,false);
-				source.addEventListener("message",WWmessageFrom,false);
-
-				WWmessageFrom(evt);
+				onMessage(evt);
 			}
 		}
 
-		function WWconnect(evt) {
+		function onConnect(evt) {
+			self.removeEventListener("connect",onConnect,false);
+
 			var port = evt.ports[0];
-			WWsetup(port,port);
+			setup(port,port);
 			port.start();
-			self.removeEventListener("connect",WWconnect,false);
 		}
 
-		function WWstart(evt) {
+		function onStart(evt) {
 			try {
 				var msg = JSON.parse(evt.data);
 			}
 			catch (err) { return; }
 
 			if (msg.start) {
-				self.removeEventListener("message",WWstart,false);
-				WWsetup(self,self);
+				self.removeEventListener("message",onStart,false);
+				setup(self,self);
 			}
 		}
-	}
-
-	function channelPendingEntry(chan) {
-		chan.pr = new Promise(function executor(resolve,reject){
-			chan.resolve = resolve;
-			chan.reject = reject;
-		});
-		return chan;
 	}
 
 });
--- index.js
+++ index.js
@@ -6,13 +6,18 @@
 	"use strict";
 
 	var bridges = {},
+		channel_bridge = {},
+		message_ids = {},
+		pre_channel_queue = {},
+		wrap_csp_return,
 		orig_csp,
 		rejected_pr = Promise.reject(),
 		publicAPI = {
-			hijackCSP: hijackCSP,
+			initCSP: initCSP,
 			defineBridge: defineBridge,
 			openChannel: openChannel
 		};
+
 
 	// opting out of false "unhandled rejection" warnings
 	rejected_pr.catch(function noop(){});
@@ -22,17 +27,81 @@
 
 	// ***************************
 
+	function defaultPromiseWrap(v) {
+		return Promise.resolve(v);
+	}
+
+	function initCSP(CSP,wrapCSPReturn) {
+		wrap_csp_return = wrapCSPReturn || defaultPromiseWrap;
+
+		// save existing API methods
+		orig_csp = {
+			take: CSP.take,
+			put: CSP.put,
+			alts: CSP.alts,
+			takem: CSP.takem,
+			takeAsync: CSP.takeAsync,
+			takemAsync: CSP.takemAsync,
+			putAsync: CSP.putAsync,
+			altsAsync: CSP.altsAsync
+		};
+
+		// hijack CSP API with wrapped methods
+		Object.keys(orig_csp).forEach(function eacher(key){
+			CSP[key] = makeCSPMethodWrapper(key,orig_csp);
+		});
+
+
+		// ***************************
+
+		function makeCSPMethodWrapper(methodName,orig) {
+			return function $$wrapper(ch) {
+				var args = [].slice.call(arguments);
+
+				if (
+					(methodName == "alts" || methodName == "altsAsync") &&
+					Array.isArray(ch)
+				) {
+					for (var i=0; i<ch.length; i++) {
+						if (ch[i].remote) {
+							ch = ch[i];
+							break;
+						}
+					}
+				}
+
+				if (ch.remote && bridges[ch.remote[0]]) {
+					args.unshift(
+						/*bridgeName=*/ch.remote[0],
+						/*channelID=*/ch.remote[1],
+						/*cspMethod=*/methodName
+					);
+					return signalChannel.apply(null,args);
+				}
+				else {
+					return orig[methodName].apply(null,args);
+				}
+			};
+		}
+	}
+
 	function defineBridge(bridgeName,handlers) {
-		bridges[bridgeName] = handlers;
+		bridges[bridgeName] = {
+			channels: {},
+			sendMessage: handlers.sendMessage
+		};
+
+		handlers.onMessage(onMessage);
 	}
 
 	function openChannel(bridgeName,channelID,bufSize) {
 		var pr, ch;
 
 		if (bridges[bridgeName]) {
+			channel_bridge[channelID] = bridgeName;
 			ch = getChannel(bufSize || 0);
 			ch.remote = [bridgeName,channelID];
-			pr = bridges[bridgeName].open(ch);
+			pr = connectChannel(ch);
 		}
 
 		return (pr || rejected_pr);
@@ -49,7 +118,7 @@
 
 			// wrap close method: also signal remote close
 			ch.close = function $$close() {
-				bridges[bridgeName].close(channelID);
+				closeChannel(bridgeName,channelID);
 				ch.origClose();
 			};
 
@@ -57,58 +126,372 @@
 		}
 	}
 
-	function hijackCSP(csp) {
-		// save existing API methods
-		orig_csp = {
-			take: csp.take,
-			put: csp.put,
-			alts: csp.alts,
-			takem: csp.takem,
-			takeAsync: csp.takeAsync,
-			takemAsync: csp.takemAsync,
-			putAsync: csp.putAsync,
-			altsAsync: csp.altsAsync
-		};
+	function connectChannel(ch) {
+		var msg, bridge_name = ch.remote[0],
+			channel_id = ch.remote[1],
+			chan_entry = bridges[bridge_name].channels[channel_id];
 
-		Object.assign(csp,{
-			take: makeCSPMethodWrapper("take",orig_csp),
-			put: makeCSPMethodWrapper("put",orig_csp),
-			alts: makeCSPMethodWrapper("alts",orig_csp),
-			takem: makeCSPMethodWrapper("takem",orig_csp),
-			takeAsync: makeCSPMethodWrapper("takeAsync",orig_csp),
-			takemAsync: makeCSPMethodWrapper("takemAsync",orig_csp),
-			putAsync: makeCSPMethodWrapper("putAsync",orig_csp),
-			altsAsync: makeCSPMethodWrapper("altsAsync",orig_csp)
+		// first time this channel-ID is trying to be opened?
+		if (!chan_entry || chan_entry.resolve) {
+			// no local channel entry yet?
+			if (!chan_entry) {
+				chan_entry = bridges[bridge_name].channels[channel_id] =
+					channelPendingEntry({ ch: ch });
+			}
+			// otherwise, remote channel connection already received
+			else {
+				chan_entry.ch = ch;
+				chan_entry.resolve(ch);
+				chan_entry.resolve = chan_entry.reject = null;
+			}
+
+			messageTo(bridge_name,{
+				"remote-connected": channel_id
+			});
+
+			if (pre_channel_queue[channel_id]) {
+				while ((msg = pre_channel_queue[channel_id].shift())) {
+					onMessage(msg);
+				}
+				delete pre_channel_queue[channel_id];
+			}
+		}
+
+		if (!ch.pending_messages) {
+			ch.pending_messages = [];
+		}
+
+		return chan_entry.pr;
+	}
+
+	function closeChannel(bridgeName,channelID) {
+		messageTo(bridgeName,{
+			"remote-closed": channelID
 		});
 	}
 
-	function makeCSPMethodWrapper(methodName,orig) {
-		return function $$wrapper(ch) {
-			var args = [].slice.call(arguments);
+	function signalChannel(bridgeName,channelID,cspMethod) {
+		var in_out = /^put/.test(cspMethod) ? "out" : "in",
+			ch = bridges[bridgeName].channels[channelID].ch,
+			args = [].slice.call(arguments,3), args2,
+			msg_entry, id, pr, pr2, ack, msg;
 
-			if (
-				(methodName == "alts" || methodName == "altsAsync") &&
-				Array.isArray(ch)
-			) {
-				for (var i=0; i<ch.length; i++) {
-					if (ch[i].remote) {
-						ch = ch[i];
-						break;
+		// found a matching (aka, opposite) entry?
+		msg_entry = findOppositePendingMessageEntry(ch,in_out);
+		if (msg_entry) {
+			id = msg_entry.in || msg_entry.out;
+
+			// local entry?
+			if (message_ids[id]) {
+				// cancel the previous remote pending message
+				messageTo(bridgeName,{
+					"remote-action-cancel": channelID,
+					"id": id
+				});
+
+				// find and remove this entry from the queue
+				findPendingMessageEntry(ch,id,/*remove=*/true);
+
+				// fake entry state as if remote signaling had completed
+				msg_entry[in_out] = getNewMessageID(in_out + ":" + bridge_name);
+
+				args2 = [ch].concat(msg_entry.waiting_for_ack.args);
+
+				// redo previously remoted action as local instead
+				pr = wrap_csp_return(orig_csp[msg_entry.waiting_for_ack.method].apply(null,args2));
+
+				// perform current CSP action as local
+				pr2 = wrap_csp_return(orig_csp[cspMethod].apply(null,args));
+
+				// compose both local csp actions together
+				pr2
+					.then(
+						function onFulfilled(){ return pr; },
+						function onRejected(){ return pr; }
+					)
+					.then(msg_entry.resolve,msg_entry.reject)
+					.then(cleanupEntry,cleanupEntry);
+
+				return pr2;
+			}
+			// otherwise, prepare ACK to send back to remote
+			else {
+				ack = id;
+			}
+		}
+
+		if (!msg_entry) {
+			// make a new (pending) local entry
+			msg_entry = makePendingMessageEntry(ch,in_out);
+			id = msg_entry.in || msg_entry.out;
+		}
+		else {
+			// add local id to previous entry
+			id = getNewMessageID(in_out + ":" + bridgeName);
+			msg_entry[in_out] = id;
+		}
+
+		// CSP method with channel as single first param
+		if (/^(?:put|take)/.test(cspMethod)) {
+			args.shift();
+		}
+		else {
+			// TODO: filter out channels from alts(..) / altsAsync(..) args
+		}
+
+		msg = {
+			"remote-action": channelID,
+			"id": id,
+			"method": cspMethod,
+			"args": args
+		};
+
+		// send ack along with message
+		if (ack) {
+			msg.ack = ack;
+		}
+
+		messageTo(bridgeName,msg);
+
+		msg_entry.waiting_for_ack = msg;
+
+		return msg_entry.pr;
+
+
+		// *******************************
+
+		function cleanupEntry() {
+			clearMessageEntry(msg_entry);
+		}
+	}
+
+	function clearMessageEntry(msgEntry) {
+		delete message_ids[msgEntry.in];
+		delete message_ids[msgEntry.out];
+		msgEntry.resolve = msgEntry.reject = msgEntry.pr = msgEntry.waiting_for_ack =
+			msgEntry.ack = msgEntry.in = msgEntry.out = null;
+	}
+
+	function findPendingMessageEntry(ch,msgID,remove) {
+		var in_out = msgID.match(/^(in|out):/)[1], msg_entry;
+
+		for (var i=0; i<ch.pending_messages.length; i++) {
+			if (ch.pending_messages[i][in_out] == msgID) {
+				msg_entry = ch.pending_messages[i];
+				if (remove) {
+					ch.pending_messages.splice(i,1);
+				}
+				return msg_entry;
+			}
+		}
+	}
+
+	function findOppositePendingMessageEntry(ch,inOut) {
+		for (var i=0; i<ch.pending_messages.length; i++) {
+			if (!ch.pending_messages[i][inOut]) {
+				return ch.pending_messages[i];
+			}
+		}
+	}
+
+	function getNewMessageID(prefix) {
+		do { var id = prefix + ":" + Math.random(); }
+		while (id in message_ids);
+		message_ids[id] = true;
+		return id;
+	}
+
+	function makePendingMessageEntry(ch,inOut) {
+		var bridge_name = ch.remote[0],
+			id = getNewMessageID(inOut + ":" + bridge_name);
+
+		var msg_entry = { in: null, out: null };
+		msg_entry[inOut] = id;
+		msg_entry.pr = new Promise(function executor(resolve,reject){
+			msg_entry.resolve = resolve;
+			msg_entry.reject = reject;
+		});
+		ch.pending_messages.push(msg_entry);
+
+		return msg_entry;
+	}
+
+	function messageTo(bridgeName,msg) {
+		bridges[bridgeName].sendMessage(msg);
+	}
+
+	function onMessage(msg) {
+		var action, channel_id, args, msg_entry, in_out,
+			bridge_name, chan_entry;
+
+		if (("remote-connected" in msg) || ("remote-closed" in msg)) {
+			action = ("remote-connected" in msg) ?
+				"remote-connected" :
+				"remote-closed";
+			channel_id = msg[action];
+
+			// bridge+channel opened locally?
+			if (channel_bridge[channel_id]) {
+				bridge_name = channel_bridge[channel_id];
+				chan_entry = bridges[bridge_name].channels[channel_id];
+
+				// remote channel connection signal?
+				if (action == "remote-connected") {
+					// local channel entry not yet defined?
+					if (!chan_entry) {
+						chan_entry = bridges[bridge_name].channels[channel_id] =
+							channelPendingEntry({});
 					}
+					// local channel still pending on remote open?
+					else if (chan_entry.resolve) {
+						chan_entry.resolve(chan_entry.ch);
+						chan_entry.resolve = chan_entry.reject = null;
+					}
+					// otherwise, ignore connection message on already
+					// opened channel
+				}
+				// remote channel close signal received?
+				else if (action == "remote-closed" &&
+					chan_entry
+				) {
+					// local channel to close?
+					if (chan_entry.ch) {
+						chan_entry.ch.origClose();
+					}
+					// local channel still pending?
+					else if (chan_entry.reject) {
+						chan_entry.reject();
+					}
+
+					// cleanup
+					chan_entry.resolve = chan_entry.reject = null;
 				}
 			}
-
-			if (ch.remote && bridges[ch.remote[0]]) {
-				args.unshift(
-					/*channelID=*/ch.remote[1],
-					/*cspMethod=*/methodName
-				);
-				return bridges[ch.remote[0]].signal.apply(null,args);
-			}
+			// defer message while bridge+channel is not yet open
 			else {
-				return orig[methodName].apply(null,args);
+				pre_channel_queue[channel_id] = pre_channel_queue[channel_id] || [];
+				pre_channel_queue[channel_id].push(msg);
 			}
-		};
+		}
+		else if (msg["remote-action"]) {
+			channel_id = msg["remote-action"];
+			bridge_name = channel_bridge[channel_id];
+			chan_entry = bridges[bridge_name].channels[channel_id];
+
+			// recognized local CSP method?
+			if (orig_csp[msg.method]) {
+				// check for opposite matching entry to attach to
+				in_out = msg.id.match(/^(in|out):/)[1];
+				msg_entry = findOppositePendingMessageEntry(chan_entry.ch,in_out);
+				if (msg_entry) {
+					// add remote msg id
+					msg_entry[in_out] = msg.id;
+
+					// send ack
+					messageTo(bridge_name,{
+						"remote-action-ack": channel_id,
+						id: msg.id
+					});
+
+					// ack bundled in with message, and entry now complete?
+					if (msg.ack && msg_entry.in && msg_entry.out) {
+						// remove the entry
+						msg_entry = findPendingMessageEntry(
+							chan_entry.ch,
+							msg.ack,
+							/*remove=*/true
+						);
+
+						// double check that we actually found/removed the entry?
+						if (msg_entry) {
+							// add channel to list of args
+							args = [
+								chan_entry.ch
+							].concat(msg_entry.waiting_for_ack.args);
+
+							// run CSP action for now-ack'd local msg
+							wrap_csp_return(
+								orig_csp[msg_entry.waiting_for_ack.method].apply(null,args)
+							)
+							.then(msg_entry.resolve,msg_entry.reject);
+
+							clearMessageEntry(msg_entry);
+						}
+					}
+				}
+				// no entry found, so make one to wait for local action
+				else {
+					msg_entry = { in: null, out: null };
+					msg_entry[in_out] = msg.id;
+					chan_entry.ch.pending_messages.push(msg_entry);
+				}
+
+				// add channel to list of args
+				args = [
+					chan_entry.ch
+				].concat(msg.args);
+
+				// run CSP action from remote msg
+				orig_csp[msg.method].apply(null,args);
+			}
+		}
+		else if (msg["remote-action-ack"]) {
+			channel_id = msg["remote-action-ack"];
+			bridge_name = channel_bridge[channel_id];
+			chan_entry = bridges[bridge_name].channels[channel_id];
+
+			msg_entry = findPendingMessageEntry(
+				chan_entry.ch,
+				msg.id
+			);
+
+			// entry complete now that is has ack and both in/out id's?
+			if (msg_entry.in && msg_entry.out) {
+				// remove the entry
+				msg_entry = findPendingMessageEntry(
+					chan_entry.ch,
+					msg.id,
+					/*remove=*/true
+				);
+
+				// add channel to list of args
+				args = [
+					chan_entry.ch
+				].concat(msg_entry.waiting_for_ack.args);
+
+				// run CSP action for now-ack'd local msg
+				wrap_csp_return(
+					orig_csp[msg_entry.waiting_for_ack.method].apply(null,args)
+				)
+				.then(msg_entry.resolve,msg_entry.reject);
+
+				clearMessageEntry(msg_entry);
+			}
+		}
+		else if (msg["remote-action-cancel"]) {
+			channel_id = msg["remote-action-cancel"];
+			bridge_name = channel_bridge[channel_id];
+			chan_entry = bridges[bridge_name].channels[channel_id];
+
+			if (chan_entry.ch) {
+				msg_entry = findPendingMessageEntry(
+					chan_entry.ch,
+					msg["id"],
+					/*remove=*/true
+				);
+				clearMessageEntry(msg_entry);
+
+				// TODO: remove the local channel's queue action!
+			}
+		}
+	}
+
+	function channelPendingEntry(chan) {
+		chan.pr = new Promise(function executor(resolve,reject){
+			chan.resolve = resolve;
+			chan.reject = reject;
+		});
+		return chan;
 	}
 
 });
